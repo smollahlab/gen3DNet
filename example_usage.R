af271db7f838958cb66feea7b551ef53c18d98e9
Morrison <paul.morrison@compute1-client-1.ris.wustl.edu>
2021-01-11 19:42:08 -0600

    clean up example_usage.R

--- example_usage.R
+++ example_usage.R
@@ -7,6 +7,7 @@
 #write.csv(histon_data, histon_path)
 #write.csv(phospho_data, phospho_path)
 
+install.packages(c("NMF", "plsdof","MASS","NbClust","cli","progress"))
 install.packages(".",type="source",repos=NULL)
 library("gen3DNet")
 
@@ -17,7 +18,12 @@ result <- gen3DNet(
     histon_path,
     phospho_path,
     nmf_nrun=10,
-    k_picker=max_cophenetic
+    #k_picker=max_cophenetic
+    #k_picker=kneedle_silhouette_consensus
+    #k_picker=kneedle_cophenetic 
+    #k_picker=max_silhouette_consensus
+    #k_picker=max_cophenetic
+    k_picker=max_ward_kl
 )
 #result <- gen3DNet(
 #    histon_path, 
